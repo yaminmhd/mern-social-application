8fd3a83c7ba07ab0900d49de6d9ebb5dede02676
yaminmhd <mdyamin.drzee@gmail.com>
2018-10-22 14:16:21 +0800

    add Post model

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const postSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
